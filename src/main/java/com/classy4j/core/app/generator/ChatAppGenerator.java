b9deb4f2cea7293344fe7486cbe170476d55e7aa
daichangya <daichangya@163.com>
2025-04-08 13:14:00 +0800

    add chat

--- src/main/java/com/classy4j/core/app/generator/ChatAppGenerator.java
+++ src/main/java/com/classy4j/core/app/generator/ChatAppGenerator.java
@@ -3,21 +3,28 @@ package com.classy4j.core.app.generator;
 import com.classy4j.config.LLMConfig;
 import com.classy4j.model.*;
 import com.classy4j.service.ProviderService;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.SystemMessage;
-import dev.langchain4j.data.message.UserMessage;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.response.ChatResponse;
-import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import com.google.common.collect.Lists;
+import io.github.pigmesh.ai.deepseek.core.OpenAiClient;
 import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.Generation;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
+import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.stereotype.Service;
 import org.testcontainers.shaded.com.google.common.collect.Maps;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 /**
  * @author changyadai
@@ -27,7 +34,6 @@ public class ChatAppGenerator {
 
     @Resource
     private ProviderService providerService;
-
 
     public CompletionResponse generate(CompletionRequest request) {
         // TODO: Implement chat generation logic
@@ -39,20 +45,19 @@ public class ChatAppGenerator {
         String provider = modelConfig.getModel().getProvider();
         Provider providerInfo =providerService.findByProviderName(provider);
         LLMConfig config = providerInfo.getEncryptedConfig();
-        ChatLanguageModel chatModel = OpenAiChatModel.builder()
-                .baseUrl(config.getEndpointUrl())
-                .apiKey(config.getApiKeyDecrypt())
-                .modelName(modelConfig.getModel().getName())
-                .logRequests(true)
-                .logResponses(true)
-                .build();
-        List<ChatMessage> messages = new ArrayList<>();
-        SystemMessage systemMessage = SystemMessage.from(modelConfig.getPrePrompt());
-        messages.add(systemMessage);
-        UserMessage userMessage = UserMessage.from(request.getQuery());
-        messages.add(userMessage);
-        ChatResponse chatResponse = chatModel.chat(messages);
-        String answer = chatResponse.aiMessage().text();
+
+        // 创建OpenAI客户端
+        OpenAiApi openAiApi = new OpenAiApi(config.getEndpointUrl(),config.getApiKeyDecrypt());
+
+        // 动态创建OpenAiChatModel
+        OpenAiChatOptions customOptions = OpenAiChatOptions.builder()
+                .model(modelConfig.getModel().getName()).build();
+        OpenAiChatModel chatModel = new OpenAiChatModel(openAiApi,customOptions);
+
+
+        // 生成回复
+        String  answer= chatModel.call(new SystemMessage(modelConfig.getPrePrompt()),new UserMessage(request.getQuery()));
+
         Map<String, Object> response = Maps.newHashMap();
         response.put("answer", answer);
         return response;
